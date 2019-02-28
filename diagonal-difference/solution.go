6d0b0d07659504a1a4f6a36156f316550c63a9e7
Sergey Lagutin <sergiula@MacBook-Air-Sergey-240.local>
2019-02-28 20:27:51 +0300

    solve diagonal-difference problem

---
+++ diagonal-difference/solution.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func abs(x int32) int32 {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+// Complete the diagonalDifference function below.
+func diagonalDifference(arr [][]int32) int32 {
+	var primary int32
+	var secondary int32
+	count := int32(len(arr)) - 1
+
+	for i, row := range arr {
+		primary += row[int32(i)]
+		secondary += row[count-int32(i)]
+	}
+	return abs(primary - secondary)
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 1024*1024)
+
+	nTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	var arr [][]int32
+	for i := 0; i < int(n); i++ {
+		arrRowTemp := strings.Split(readLine(reader), " ")
+
+		var arrRow []int32
+		for _, arrRowItem := range arrRowTemp {
+			arrItemTemp, err := strconv.ParseInt(arrRowItem, 10, 64)
+			checkError(err)
+			arrItem := int32(arrItemTemp)
+			arrRow = append(arrRow, arrItem)
+		}
+
+		if len(arrRow) != int(n) {
+			panic("Bad input")
+		}
+
+		arr = append(arr, arrRow)
+	}
+
+	result := diagonalDifference(arr)
+
+	fmt.Fprintf(writer, "%d\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
